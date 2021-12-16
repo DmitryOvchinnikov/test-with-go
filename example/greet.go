4b6302794faa3bfe1432cc9943e1df2336460364
Dmitry Ovchinnikov <dmitry.v.ovchinnikov@gmail.com>
2021-12-16 16:32:53 +0300

    5.3 Unordered example output.

--- example/greet.go
+++ example/greet.go
@@ -10,3 +10,13 @@ func (d Demo) Hello() {}
 func Hello(name string) (string, error) {
 	return fmt.Sprintf("Hello, %s", name), nil
 }
+
+// Page will print out a message asking each person
+// who hasn't checked in to do so.
+func Page(checkIns map[string]bool) {
+	for name, checkIn := range checkIns {
+		if !checkIn {
+			fmt.Printf("Paging %s; please see the front desk to check in.\n", name)
+		}
+	}
+}
--- example/greet_test.go
+++ example/greet_test.go
@@ -3,7 +3,7 @@ package example
 
 import "fmt"
 
-func ExampleHello_name() {
+func ExampleHello() {
 	greeting, err := Hello("Jon")
 	if err != nil {
 		panic(err)
@@ -12,4 +12,21 @@ func ExampleHello_name() {
 
 	// Output:
 	// Hello, Jon
+}
+
+func ExamplePage() {
+	checkIns := map[string]bool{
+		"Bob":   true,
+		"Alice": false,
+		"Eve":   false,
+		"Janet": true,
+		"Susan": true,
+		"John":  false,
+	}
+	Page(checkIns)
+
+	// Unordered output:
+	// Paging Alice; please see the front desk to check in.
+	// Paging Eve; please see the front desk to check in.
+	// Paging John; please see the front desk to check in.
 }
