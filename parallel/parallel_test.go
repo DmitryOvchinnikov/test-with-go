9e229e8096eb408d8c3e5567c053fc5081b7d1f6
Dmitry Ovchinnikov <dmitry.v.ovchinnikov@gmail.com>
2021-12-21 14:24:49 +0300

    7.4 Gotchas with closures and parallel tests - bad result.

--- parallel/parallel_test.go
+++ parallel/parallel_test.go
@@ -3,35 +3,13 @@ package parallel
 import (
 	"fmt"
 	"testing"
-	"time"
 )
 
-func TestSomething(t *testing.T) {
-	t.Parallel()
-	time.Sleep(time.Second)
-}
-
-func TestA(t *testing.T) {
-	t.Parallel()
-	time.Sleep(time.Second)
-}
-
-func TestB(t *testing.T) {
-	fmt.Println("setup")
-	defer fmt.Println("deferred teardown")
-	t.Run("group", func(t *testing.T) {
-		t.Run("sub1", func(t *testing.T) {
+func TestGotcha(t *testing.T) {
+	for i := 0; i < 10; i++ {
+		t.Run(fmt.Sprintf("i=%d", i), func(t *testing.T) {
 			t.Parallel()
-			// run sub1
-			time.Sleep(time.Second)
-			fmt.Println("sub1 done")
+			t.Logf("Testing with i=%d", i)
 		})
-		t.Run("sub2", func(t *testing.T) {
-			t.Parallel()
-			// run sub2
-			time.Sleep(time.Second)
-			fmt.Println("sub2 done")
-		})
-	})
-	fmt.Println("teardown")
+	}
 }
